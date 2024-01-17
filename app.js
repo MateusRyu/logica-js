f84b4c4551d3804125166a426ed2fd8cd6594021
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-17 20:50:53 -0300

    feat: Adiciona dicas para adivinhar o número secreto a cada turno de  adivinhação

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ while (numeroSecreto != chute) {
   chute = prompt('Escolha um número entre 1 e 10');
   console.log(`Número secreto: ${numeroSecreto}`);
   console.log(`Chute: ${chute}`);
+  if (numeroSecreto > chute){
+    alert(`O número secreto é maior que ${chute}.`);
+  } else if (numeroSecreto < chute) {
+    alert(`O número secreto é menor que ${chute}.`)
+  }
 }
 
 alert(`Parabéns! Você descobriu o número secreto (${numeroSecreto}).`);
