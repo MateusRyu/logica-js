4517ad6168619413184669ebc52a358656533432
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-17 20:28:24 -0300

    feat: Cria a estrutura básica do jogo do número secreto

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+let chute;
+
+while (numeroSecreto != chute) {
+  chute = prompt('Escolha um número entre 1 e 10');
+  console.log(`Número secreto: ${numeroSecreto}`);
+  console.log(`Chute: ${chute}`);
+}
+
