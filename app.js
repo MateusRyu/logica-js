9e3ecd6236c5d4d08c9b72b705fb1a9555f71cb1
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-17 21:02:47 -0300

    feat: Adiciona um contador de tentativas

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 0;
 
 while (numeroSecreto != chute) {
+  tentativas++;
   chute = prompt('Escolha um número entre 1 e 10');
   console.log(`Número secreto: ${numeroSecreto}`);
   console.log(`Chute: ${chute}`);
+  console.log(`Tentativas: ${tentativas}`);
   if (numeroSecreto > chute){
     alert(`O número secreto é maior que ${chute}.`);
   } else if (numeroSecreto < chute) {
@@ -14,5 +17,5 @@ while (numeroSecreto != chute) {
   }
 }
 
-alert(`Parabéns! Você descobriu o número secreto (${numeroSecreto}).`);
+alert(`Parabéns! Você descobriu o número secreto '${numeroSecreto}' com ${tentativas} tentativa(s).`);
 
