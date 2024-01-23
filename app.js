0ab7d38d61da843d20aedee2f08c1ba91ea976c9
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-22 22:33:35 -0300

    refactor: Generaliza o algoritmo para exibiir mensagem para o usuario

--- app.js
+++ app.js
@@ -9,36 +9,33 @@ let numeroSecreto;
 let texto;
 let tentativas;
 
+function exibeMensagem(mensagem, elemento) {
+    elemento.innerText = mensagem;
+    responsiveVoice.speak(mensagem);
+}
+
 function verificarChute() {
     tentativas++;
     if (numeroSecreto == chute.value) {
         exibeVitoria();
     } else if (numeroSecreto > chute.value){
-        texto = `O número secreto é maior que ${chute.value}.`;
-        dica.innerText = texto;
-        responsiveVoice.speak(texto);
+        exibeMensagem(`O número secreto é maior que ${chute.value}.`, mensagem);
     } else if (numeroSecreto < chute.value) {
-        texto = `O número secreto é menor que ${chute.value}.`;
-        dica.innerText = texto;
-        responsiveVoice.speak(texto);
+        exibeMensagem(`O número secreto é menor que ${chute.value}.`, mensagem);
     }
 }
 
 function reiniciarJogo() {
     numeroMaximo = 100;
     numeroSecreto = parseInt(Math.random() * numeroMaximo) + 1;
-    texto = `Escolha um número entre 1 a ${numeroMaximo}:`;
-    mensagem.innerText = texto;
-    responsiveVoice.speak(texto);
+    exibeMensagem(`Escolha um número entre 1 a ${numeroMaximo}:`, mensagem);
     tentativas = 0;  
 }
 
 function exibeVitoria() {
     let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
-    texto = `Parabéns! Você descobriu o número secreto '${numeroSecreto}' com ${tentativas} ${palavraTentativas}.`;
+    exibeMensagem(`Parabéns! Você descobriu o número secreto '${numeroSecreto}' com ${tentativas} ${palavraTentativas}.`, mensagem);
     dica.innerText = "";
-    mensagem.innerText = texto;
-    responsiveVoice.speak(texto);
 }
 
 responsiveVoice.speak("Bem-vindo ao jogo: Adivinhe o número secreto!");
